f31e666b9d2ca69cc1c9c527cf0f17859283c04c
bilal-aslam96 <bilal-aslam@ashlarglobal.com>
2022-06-02 18:04:27 +0500

    ui

--- app/src/main/java/com/example/cleanarchitecture/ui/ShoppingFragment.kt
+++ app/src/main/java/com/example/cleanarchitecture/ui/ShoppingFragment.kt
@@ -6,16 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.cleanarchitecture.R
+import kotlinx.android.synthetic.main.fragment_shopping.*
 
 
-class ShoppingFragment : Fragment() {
+class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
 
     lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
-    }
 
+        fabAddShoppingItem.setOnClickListener {
+//            findNavController().navigate(ShoppingFr
+//            )
+        }
+    }
 }
